f520be557d22f551fce87519f3dac4a1471e3067
Shalini Bhowmik <shalinibhowmik61@gmail.com>
2026-09-24 14:07:55 +0530

    Update db.js with Aiven SSL and port configuration

--- backend/db.js
+++ backend/db.js
@@ -5,18 +5,19 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 20011,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
-
     if (err) {
         console.log("Database connection failed:", err);
         return;
     }
-
     console.log("Quizly MySQL database connected successfully!");
-
 });
 
 module.exports = db;
